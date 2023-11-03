e08e29569cd61e4969f6d5c8a65fd5dbfe557c25
kiran <kirangunna09@gmail.com>
2023-11-03 10:42:08 +0530

    commit

--- retrive all tables from  a db.sql
+++ retrive all tables from  a db.sql
@@ -2,6 +2,8 @@
  
  use northwind_database   ---repalce ab with preferred database 
  
+ ----approach 1
+
  IF OBJECT_ID('tempdb..#tmp12') IS NOT NULL
     DROP TABLE #tmp12
 
@@ -24,3 +26,24 @@ select top 1 @tablename=  tablename from #tmp12
   select @row=count(*) from #tmp12
 
 end
+
+
+
+----------appraoch  2
+
+/*declare  c2 cursor  for select [name] from sys.tables
+
+open c2
+declare @name nvarchar(225)
+declare @sql nvarchar(max) 
+fetch next from c2 into @name 
+while @@FETCH_STATUS=0
+begin 
+select @name as tableName 
+ set @sql='select * from'+quotename( @name)
+  exec sp_executesql @sql
+  fetch next from c2 into @name 
+ end 
+ close c2
+ deallocate c2*/
+ 
