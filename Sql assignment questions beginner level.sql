c6fa2dbbe068eec74c2593dcae8491283e63e80a
kiran <kirangunna09@gmail.com>
2023-11-06 19:07:09 +0530

    commit

--- Sql assignment questions beginner level.sql
+++ Sql assignment questions beginner level.sql
@@ -1,118 +1,164 @@
 --Q.1
 
-select count(distinct companyname) as total_customers from customers
-select count(*) from customers
+SELECT COUNT(DISTINCT companyname) AS total_customers FROM customers
+SELECT COUNT(*) FROM customers
 
 --Q.2
-select companyname from customers where CompanyName like 'b%'
+SELECT companyname FROM customers WHERE CompanyName LIKE 'b%'
 
 --Q.3
 
-select companyname from customers where CompanyName   like '%s%'
+SELECT companyname FROM customers WHERE CompanyName   LIKE '%s%'
 
 --Q.4
 
-select city,
-	   count(customerid) as noOfCustomers
-	   from customers
-	   group by city 
-	   order by noOfCustomers 
+SELECT city,
+	   COUNT(customerid) AS noOfCustomers
+	   FROM customers
+	   GROUP BY city 
+	   ORDER BY noOfCustomers 
 
 --Q.5
 
-select top 3  city,
-	   count(customerid) as noOfCustomers
-	   from customers
-	   group by city 
-	   order by noOfCustomers desc
+SELECT	TOP 3  city,
+	   COUNT(customerid) AS noOfCustomers
+	   FROM customers
+	   GROUP BY city 
+	   ORDER BY noOfCustomers DESC
 	       --or---
 
- with cte as 
-(select city,
-	   count(customerid) as noOfCustomers,
-	   ROW_NUMBER() over (order by  count(customerid) desc) as rownumber
-	   from customers
-	   group by city )
+ with cte AS 
+(SELECT city,
+	   COUNT(customerid) AS noOfCustomers,
+	   ROW_NUMBER() OVER (ORDER BY  COUNT(customerid) DESC) AS rownumber
+	   FROM customers
+	   GROUP BY city )
 
-	   select city, noOfCustomers from cte where rownumber<4
+	   SELECT city, noOfCustomers FROM cte WHERE rownumber<4
 
 --Q.6
 
-with cte as 
-( select  c.CustomerID,
+WITH cte AS 
+( SELECT  c.CustomerID,
 	 CompanyName,
 	 ContactName,
-	 count(orderid ) as totalOrders,
-	 ROW_NUMBER() over (order by  count(orderid ) desc) as rownum from [dbo].[Customers] c
- join orders o  on c.CustomerID=o.CustomerID
- group by c.CustomerID,CompanyName,ContactName)
+	 COUNT(orderid ) AS totalOrders,
+	 ROW_NUMBER() OVER (ORDER BY  COUNT(orderid ) DESC) AS rownum FROM [dbo].[Customers] c
+ JOIN orders o  ON  c.CustomerID=o.CustomerID
+ GROUP BY c.CustomerID,CompanyName,ContactName)
  
- select CustomerID,
+ SELECT CustomerID,
 	 CompanyName,
 	 ContactName
-from cte where rownum=1 
+FROM cte WHERE rownum=1 
 
 
 --Q.7
 
 
-with cte as 
-( select  c.CustomerID,
+WITH cte AS 
+( SELECT  c.CustomerID,
 	 CompanyName,
 	 ContactName,orderdate,
-	 count(orderid ) as totalOrders,
-	 ROW_NUMBER() over (order by  count(orderid ) desc) as rownum from [dbo].[Customers] c
- join orders o  on c.CustomerID=o.CustomerID
- group by c.CustomerID,CompanyName,ContactName,orderdate)
+	 COUNT(orderid ) AS totalOrders,
+	 ROW_NUMBER() OVER (ORDER BY  COUNT(orderid ) DESC) AS rownum FROM [dbo].[Customers] c
+ JOIN orders o  ON  c.CustomerID=o.CustomerID
+ GROUP BY c.CustomerID,CompanyName,ContactName,orderdate)
  
- select CustomerID,
-	 CompanyName
-from cte where rownum=1 and  orderdate like '%1997%'
+	SELECT CustomerID,
+		   CompanyName
+	FROM cte WHERE rownum=1 and  orderdate LIKE '%1997%'
 
 
 --Q.8
 
-select top 3 country,
-		count(orderid) noOfOrders
-from customers c 
-join orders o on c.CustomerID=o.CustomerID
-group by country
-order by noOfOrders desc
+SELECT top 3 country,
+		COUNT(orderid) noOfOrders
+FROM customers c 
+JOIN orders o ON  c.CustomerID=o.CustomerID
+GROUP BY country
+ORDER BY noOfOrders DESC
 
 --Q.9
 
-select  top 1 companyname as shippername ,
-		count(o.ShipVia) totalShipments 
-from shippers s
-join orders o on s.ShipperID=o.ShipVia
-group by CompanyName
+SELECT  top 1 companyname AS shippername ,
+		COUNT(o.ShipVia) totalShipments 
+FROM shippers s
+JOIN orders o ON  s.ShipperID=o.ShipVia
+GROUP BY CompanyName
 
 --Q.11
 
-select lastname from employees
-where DATEPART(month,birthdate)=09
+SELECT lastname FROM employees
+WHERE DATEPART(MONTH,birthdate)=09
 
 --Q.12
-select 
+SELECT 
 	firstname, 
-	lastname, 
-	territorydescription as territory 
-from employees e
-join EmployeeTerritories et on e.EmployeeID=et.EmployeeID
-join Territories t on et.TerritoryID=t.TerritoryID
-group by TerritoryDescription,LastName,FirstName
+	lAStname, 
+	territoryDESCription AS territory 
+FROM employees e
+JOIN EmployeeTerritories et ON  e.EmployeeID=et.EmployeeID
+JOIN Territories t ON  et.TerritoryID=t.TerritoryID
+GROUP BY Territorydescription,lastName,FirstName
 
 
 --Q.13
 
-with cte as (
-select 
+with cte AS (
+SELECT 
 	od.productid,
-	sum(od.UnitPrice*od.Quantity) as saleamount,
+	sum(od.UnitPrice*od.Quantity) AS saleamount,
 	productname 
-from [Order Details] od 
-join Products p on od.ProductID= p.ProductID 
-group by od.ProductID,ProductName
+FROM [Order Details] od 
+JOIN Products p ON  od.ProductID= p.ProductID 
+GROUP BY od.ProductID,ProductName
 )
 
-select  productname as topsaleproduct from cte where saleamount=(select max(saleamount) from cte)
+SELECT  productname AS topsaleproduct FROM cte WHERE saleamount=(SELECT max(saleamount) FROM cte)
+
+--Q.14
+
+SELECT 
+	p.productname,
+	o.unitprice*o.Quantity AS saleamount,
+	COUNT(o.OrderID) AS totalOrders
+FROM products p
+JOIN [Order Details] o ON  p.ProductID=o.ProductID
+WHERE o.unitprice*o.Quantity =(SELECT min (unitprice*Quantity) FROM [Order Details]) 
+GROUP BY ProductName,o.unitprice*o.Quantity
+having COUNT(o.OrderID)<=1
+
+--Q.15
+	SELECT top 1 MONTH AS BestMonthForSale FROM(SELECT 
+		datename(MONTH,o.orderDate) AS [month],
+		sum(od.unitprice*od.Quantity) AS totalSale
+		FROM orders o 
+		JOIN [Order Details] od ON  o.OrderID=od.OrderID
+		GROUP BY DATENAME(MONTH,o.orderDate)
+		) totalsalebymonth
+		ORDER BY totalSale DESC
+		
+--q.16
+	SELECT 
+		TOP 1 CONCAT(FirstName,LAStName)AS full_name,
+		od.UnitPrice*od.Quantity AS total_sale ,
+		COUNT(o.orderid)AS total_orders
+	FROM employees e 
+	JOIN orders o ON  e.EmployeeID=o.EmployeeID
+	JOIN [Order Details] od ON  o.OrderID=od.OrderID
+	GROUP BY 
+		CONCAT (FirstName,lastname),od.UnitPrice*od.Quantity
+	ORDER BY
+		od.UnitPrice*od.Quantity DESC 
+
+--Q.17
+	SELECT 
+		region AS sales_region, 
+		COUNT(employeeid)AS noOfEmployees 
+	FROM employees 
+	GROUP BY region 
+	ORDER BY COUNT(employeeid) DESC
+
+	--Q.18
+
