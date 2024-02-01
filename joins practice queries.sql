45f22cf2af2ede55007e0cbeb2b269bd696718ff
kiran <kirangunna09@gmail.com>
2024-02-01 12:20:10 +0530

    sdfsdf

--- joins practice queries.sql
+++ joins practice queries.sql
@@ -1,3 +1,62 @@
+use Company
+
+select  * from projects
+select  * from employee
+---select the employess who are in projects 
+SELECT Id as EmployeeID, 
+	   Name, 
+	   Department, 
+	   City, 
+	   Title as Project, 
+	   ClientId
+FROM Employee 
+INNER JOIN Projects 
+ON Employee.Id = Projects.EmployeeId
+
+----display the information of employees who are working on project and who are not in projects 
+
+	SELECT Id as EmployeeID, 
+		   Name, 
+		   Department, 
+		   City, 
+		   Title as Project, 
+		   ClientId
+	FROM Employee 
+	LEFT JOIN Projects 
+	ON Employee.Id = Projects.EmployeeId
+
+	----get all the project details alog with the assigned employee datails and projects that are not assigned to any employee 
+
+	SELECT Title as Project,
+		   Id as EmployeeID, 
+		   Name, 
+		   Department, 
+		   City, 
+		   ClientId
+	FROM Employee 
+	RIGHT JOIN Projects 
+	ON Employee.Id = Projects.EmployeeId
+	order by EmployeeID desc
+	
+	---get all the employe and project details 
+
+	SELECT 
+		Id as EmployeeID,
+		Name,
+		Department,
+		City,
+		Title as Project, 
+		ClientId
+	FROM Employee 
+	FULL JOIN Projects 
+	ON Employee.Id = Projects.EmployeeId
+
+
+
+
+	/********************************************************************************************************************************/
+
+use adventureworks2022
 ---retrieve the product information, including the product name, vendor name, and vendor contact details.
 
 select * from [Production].[Product]
@@ -91,7 +150,7 @@ from [Production].[Product] pro
 
 
 
-
+	 /***write a query to find the total sales by category and subcategory wise and percentage of sale in total sale ****/
 
 
 
@@ -115,3 +174,92 @@ from [Production].[Product] pro
 	  from cte
 	  group by name,subcategory,total_sale
 	  order by category
+
+	  /***find the top 3 products in each product category based on their sales amount.
+	  Include columns like ProductCategory, ProductName, SalesAmount, and the rank of each product within its category.*****/
+
+	  with cte (category, product,total_sale, rownum)
+	  as 
+	( 
+	  select 
+	  pc.name,
+	  p.name,
+	  sum(sod.OrderQty*sod.UnitPrice),
+	  row_number() over(partition by pc.name order by sum(sod.OrderQty*sod.UnitPrice) desc) 
+	  from [Production].[ProductCategory] pc
+	  join [Production].[ProductSubcategory] psc on pc.ProductCategoryID=psc.ProductCategoryID
+	  join [Production].[Product] p on psc.ProductSubcategoryID=p.ProductSubcategoryID
+	  join [Sales].[SalesOrderDetail] sod on p.ProductID=sod.ProductID
+	  group by pc.name, p.name
+	)
+	select * from cte
+	where rownum<4
+	order by category
+
+
+	/*****************************************/
+	select DATENAME(DW,1)
+	SELECT DATENAME(weekday, GETDATE()) AS DayName;
+	WITH cte_numbers(n, weekday) 
+AS (
+    SELECT 
+        0, 
+        DATENAME(DW, 0)
+    UNION ALL
+    SELECT    
+        n + 1, 
+        DATENAME(DW, n + 1)
+    FROM    
+        cte_numbers
+    WHERE n <=5
+)
+SELECT 
+    *
+FROM 
+    cte_numbers;
+
+	/********************/
+
+	-- Create a scalar function to calculate the sale amount of each product 
+alter FUNCTION dbo.totalsaleamount (
+    @OrderQty INT,
+    @UnitPrice MONEY
+)
+RETURNS MONEY
+AS
+BEGIN
+    DECLARE @toal_sale MONEY;
+
+    -- Your logic for calculating extended amount goes here
+    SET @toal_sale = @OrderQty * @UnitPrice 
+
+    RETURN @toal_sale;
+END;
+GO
+
+-- Use the function in the query to calculate total sales amount for each product
+SELECT
+    ProductID,
+    SUM(dbo.totalsaleamount(OrderQty, UnitPrice)) AS TotalSalesAmount
+FROM
+   sales.SalesOrderDetail
+GROUP BY
+    ProductID;
+
+
+/********** Create a table-valued function that takes a start date and an end date as parameters and 
+returns a table of employees hired between those dates from the HumanResources.Employee table. 
+Write a query using this function to retrieve the list of employees hired between '2010-01-01' and '2011-12-31'.   **********/
+
+select * from HumanResources.Employee
+
+create function employees_hiried(
+		@startdate date,
+		@enddate date )
+returns table
+as 
+return 
+select * from HumanResources.Employee where HireDate  between @startdate and @enddate
+ 
+SELECT * FROM employees_hiried('2010-01-01', '2011-12-31');
+
